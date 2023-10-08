8792435dcea9b9383c0834b95f70df04ce043076
ArielBetti <ariel.betti@gmail.com>
2023-10-08 04:03:13 -0300

    fix: migration to fastapi fixes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
+import psycopg2
 from pydantic import BaseModel
+from typing import List, Union
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -7,51 +9,88 @@ import numpy as np
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from a .env file
 load_dotenv()
+
+# Create a FastAPI instance
 app = FastAPI()
 
-# Coloque suas credenciais do banco de dados aqui
-DATABASE_URL = os.getenv("DB_STRING")
+# Connect to the database
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
 
-# Simula um banco de dados com dados de árvores
+# SQL query to retrieve data from the 'reflorestation_trees' table
 query = "SELECT * FROM reflorestation_trees;"
-df = pd.read_sql_query(query, DATABASE_URL)
+
+# Create a DataFrame from the query results
+df = pd.read_sql_query(query, conn)
+
+# Standardize numerical features using StandardScaler
 scaler = StandardScaler()
 df_scaled = pd.DataFrame(scaler.fit_transform(df[['grow_space', 'germination_time', 'time_to_adulthood', 'gas_ch4_reduction', 'ideal_soil_moisture', 'gas_c02_reduction']]), columns=[
                          'grow_space', 'germination_time', 'time_to_adulthood', 'gas_ch4_reduction', 'ideal_soil_moisture', 'gas_c02_reduction'])
 
-# Escolha o número de clusters
+# Close the database connection
+conn.close()
+
+# Choose the number of clusters for KMeans
 num_clusters = 1
 
-# Aplicar o algoritmo KMeans
+# Apply the KMeans algorithm
 kmeans = KMeans(n_clusters=num_clusters, random_state=42)
 df['cluster'] = kmeans.fit_predict(df_scaled)
+
+# Define the Pydantic model for a Tree
+
+
+class Tree(BaseModel):
+    name: str
+    type: str
+    recommended_quantity: int
+    grow_space: float
+    germination_time: int
+    time_to_adulthood: int
+    gas_ch4_reduction: float
+    ideal_soil_moisture: float
+    gas_c02_reduction: float
+
+# Define the Pydantic model for a Region
 
 
 class Region(BaseModel):
     region_size: int
     region_soil_moisture: int
 
+# Endpoint to recommend trees based on a region
 
-@app.post("/recommend-trees")
+
+@app.post("/recommend-trees", response_model=List[Tree])
 async def recommend_trees(region: Region):
-    # Normalizar o tamanho da região e umidade do solo
+    # Normalize the region size and soil moisture
     region_data_scaled = scaler.transform(
         [[region.region_size, 0, 0, 0, region.region_soil_moisture, 0]])
 
-    # Prever o cluster da região
+    # Predict the cluster of the region
     cluster = kmeans.predict(region_data_scaled)[0]
 
-    # Filtrar as árvores no mesmo cluster e com umidade do solo semelhante
+    # Filter trees in the same cluster with similar soil moisture
     cluster_trees = df[(df['cluster'] == cluster) & (df['ideal_soil_moisture'] >=
                                                      region.region_soil_moisture - 5) & (df['ideal_soil_moisture'] <= region.region_soil_moisture + 5)]
 
-    # Calcular a quantidade ideal de árvores com base no tamanho da região
+    # Calculate the recommended quantity of trees based on the region size
     cluster_trees['recommended_quantity'] = np.round(
         region.region_size / cluster_trees['grow_space']).astype(int)
 
-    # Ordenar as recomendações com base em critérios diversos (por exemplo, grow_space e umidade do solo)
+    # Sort the recommendations based on various criteria (e.g., grow_space and soil moisture)
     cluster_trees = cluster_trees.sort_values(
         by=['grow_space', 'ideal_soil_moisture'])
 
-    return cluster_trees[['name', 'type', 'recommended_quantity']].to_dict(orient='records')
+    # Convert the DataFrame to a list of dictionaries
+    result_trees = cluster_trees.to_dict(orient='records')
+
+    # Return the recommended trees
+    return result_trees
